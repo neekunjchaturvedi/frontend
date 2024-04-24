b72d5100bd199c9f5df5248d0302da2542a5317d
neekunjchaturvedi <121626257+neekunjchaturvedi@users.noreply.github.com>
2024-04-24 06:28:15 +0530

    Myntra

---
+++ myntra/script.js
@@ -0,0 +1,11 @@
+/*let gamenum = 25;
+while (true) {
+  let input = prompt("Guess the number");
+  if (input == gamenum) {
+    console.log("game over");
+    break;
+  }
+}
+*/
+let uname = prompt("enter your name");
+console.log("@" + uname + uname.length);
